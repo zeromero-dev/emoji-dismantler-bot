c3046f768143cfbc0388f02dc22e4b1bb3140899
Roman <spiderr.roma@gmail.com>
2022-04-06 18:41:57 +0300

    works for me tho

--- src/index.js
+++ src/index.js
@@ -8,12 +8,19 @@ const bot = new TelegramBot(token, {polling: true});
 const noEmojis = /\P{Extended_Pictographic}/u
 
 bot.on('message', (msg) => {
-    
-    console.log(msg.text)
-    console.log([...msg.text])
+    const text = msg.text
+    const chatId = msg.chat.id
+    // console.log(msg.text)
+    // console.log([...msg.text])
+    //if the text 
+    const tester = noEmojis.test(text)
+    // console.log(tester)
     const splitted = [...msg.text].filter(item => item !== "‍")
-    bot.sendMessage('hello')
-    console.log(splitted.join(' '))
+    tester === true ? bot.sendMessage(chatId, `Sorry, can't do that :(`) 
+        : bot.sendMessage(chatId, splitted.join(','))
+    
+    // bot.sendMessage(chatId, splitted.join(','))
+    // console.log(splitted.join(' '))
    
 })
 
