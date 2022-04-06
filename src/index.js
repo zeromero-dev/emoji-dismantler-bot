a83d9f381c6c8f4ab7a82147509ffaedd43e237e
Roman <spiderr.roma@gmail.com>
2022-04-06 18:45:58 +0300

    cleaned all the mess

--- src/index.js
+++ src/index.js
@@ -10,20 +10,8 @@ const noEmojis = /\P{Extended_Pictographic}/u
 bot.on('message', (msg) => {
     const text = msg.text
     const chatId = msg.chat.id
-    // console.log(msg.text)
-    // console.log([...msg.text])
-    //if the text 
     const tester = noEmojis.test(text)
-    // console.log(tester)
     const splitted = [...msg.text].filter(item => item !== "‍")
     tester === true ? bot.sendMessage(chatId, `Sorry, can't do that :(`) 
         : bot.sendMessage(chatId, splitted.join(','))
-    
-    // bot.sendMessage(chatId, splitted.join(','))
-    // console.log(splitted.join(' '))
-   
 })
-
-
-//take the emoji 
-//return the dismantled emoji
